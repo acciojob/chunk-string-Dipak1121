e0b5212d70a63fded2be92ef8a7e69561d6ef5c8
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-27 21:15:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function stringChop(str, size) {
-  // your code here
+	
+	let num  = parseInt(size);
+	  let arr = [];
+	  let i = 0;
+	  let n = str.length;
+	  while((i+num) <= n ){
+		  arr.push(str.slice(i,i+num));
+		  i = i + num;
+	  }
+	  if( i < n ){
+		arr.push(str.slice(i,i+num));
+	  }
+	  
+	  return arr;
+  }
+  
+ 
+  const str = prompt("Enter String.");
+  const size = prompt("Enter Chunk Size.");
+  alert(stringChop(str, size));
+	return arr;
 }
 
 // Do not change the code below
